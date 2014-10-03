1509856a849b28f2ea0d566481636aaa7acb41a2
mezod <mezood@gmail.com>
2014-10-04 01:44:14 +0200

    grunt, less, requirejs, minification and watch

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+//Gruntfile
+module.exports = function(grunt) {
+
+  // requirejs compile options
+  var compileOptions = {
+
+      mainConfigFile: 'app/scripts/main.js',
+      baseUrl: 'app/scripts',
+      include: ['main'],
+      out: 'dist/main.min.js',
+      removeCombined: false,
+      findNestedDependencies: true,
+
+      //Removes console.logs for production
+      onBuildWrite: function (moduleName, path, contents) {
+          if(/(.*)js\/modules\/(.*)/.test(path)) return contents.replace(/console.log(.*);/g, ';');
+          return contents;
+      }
+  }
+
+  //Initializing the configuration object
+  grunt.initConfig({
+
+    // Task configuration
+    less: {
+        development: {
+            options: {
+              compress: true,  //minifying the result
+            },
+            files: {
+              //compiling base.less into main.min.css
+              "./dist/main.min.css":"./app/styles/base.less"
+            }
+        }
+    },
+    requirejs: {
+        compile: {
+            options : compileOptions
+        }
+    },
+    watch: {
+        less: {
+            // Watch all .less files from the styles directory)
+            files: ['app/styles/*.less'],
+            tasks: ['less'],
+            // Reloads the browser
+            options: {
+              livereload: true  
+            }
+        },
+        requirejs: {
+            // Watch only main.js so that we do not constantly recompile the .js files
+            files: [ 'app/scripts/main.js' ],
+            tasks: [ 'requirejs' ],
+            // Reloads the browser
+            options: {
+              livereload: true  
+            }
+        }
+    }
+  });
+
+  // Plugin loading
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Task definition
+  grunt.registerTask('default', ['watch']);
+
+};
