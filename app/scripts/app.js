a56b6fd3fcaeb2cc632f10fdedc45ea304ed923b
mezod <mezood@gmail.com>
2015-03-11 23:28:56 +0100

    root pushstate

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,7 +34,7 @@ define(["marionette"], function(Marionette){
 					App.loggedInUser = JSON.parse(window.localStorage.getItem('multikanban user'));
 				}
 
-	        	Backbone.history.start({pushState: true, root: "/multikanban/app/"});
+	        	Backbone.history.start({pushState: true});
 
 	        	if(App.getCurrentRoute() === undefined){
 		        	console.log("undefined current route");
